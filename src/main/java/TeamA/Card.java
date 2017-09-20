9bc21cc85ad3f104711f66660c7cd5b7ba1a6dda
Theos Mariani <theos.mariani@etu.unice.fr>
2017-09-20 16:23:36 +0200

    creation de la classe card

---
+++ src/main/java/TeamA/Card.java
@@ -0,0 +1,12 @@
+package TeamA;
+
+public class Card {
+    private int valeur;
+    private String nom;
+
+    public Card(int valeur, String nom){
+        this.valeur = valeur;
+        this.nom = nom;
+    }
+
+}
