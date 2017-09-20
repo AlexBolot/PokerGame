cc098d4ff4c28f1df89db02df76aedfc5633a9f5
Theos Mariani <theos.mariani@etu.unice.fr>
2017-09-20 16:27:41 +0200

    creation du package classe

---
+++ src/main/java/TeamA/classe/Card.java
@@ -0,0 +1,13 @@
+package TeamA.classe;
+
+public class Card {
+    private int value;
+    private String name;
+
+    public Card(int value, String name){
+        this.value = value;
+        this.name = name;
+    }
+
+
+}
