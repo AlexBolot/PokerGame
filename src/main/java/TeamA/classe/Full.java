727f8a70bba61df9efd392064cbf974c79a4d0a8
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-23 15:37:38 +0200

    Adding Full class (unImplemented)

---
+++ src/main/java/TeamA/classe/Full.java
@@ -0,0 +1,21 @@
+package TeamA.classe;
+
+import java.util.ArrayList;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The Full	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 23/09/17 15:27
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+public class Full extends Combination
+{
+    public Full (ArrayList<Card> hand)
+    {
+        super(hand);
+    }
+}
