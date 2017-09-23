da060bcb6952c11363287c46c09a657b1ee409fd
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-23 15:37:16 +0200

    Adding Brelan class (unImplemented)

---
+++ src/main/java/TeamA/classe/Brelan.java
@@ -0,0 +1,21 @@
+package TeamA.classe;
+
+import java.util.ArrayList;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The Brelan	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 23/09/17 15:23
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+public class Brelan extends Combination
+{
+    public Brelan (ArrayList<Card> hand)
+    {
+        super(hand);
+    }
+}
