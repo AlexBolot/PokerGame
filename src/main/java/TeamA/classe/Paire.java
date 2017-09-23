2995051df24b4f2a8abef8e215f3de2d85b5badf
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-23 15:37:52 +0200

    Adding Paire class (unImplemented)

---
+++ src/main/java/TeamA/classe/Paire.java
@@ -0,0 +1,21 @@
+package TeamA.classe;
+
+import java.util.ArrayList;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The Paire	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 23/09/17 15:23
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+public class Paire extends Combination
+{
+    public Paire (ArrayList<Card> hand)
+    {
+        super(hand);
+    }
+}
