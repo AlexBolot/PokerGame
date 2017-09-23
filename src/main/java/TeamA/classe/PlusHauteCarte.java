417e3df90da09dab0bcec09a2bfa741432533a0c
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-23 15:23:18 +0200

    Adding PlusHauteCarte class (unImplemented)

---
+++ src/main/java/TeamA/classe/PlusHauteCarte.java
@@ -0,0 +1,21 @@
+package TeamA.classe;
+
+import java.util.ArrayList;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The PlusHauteCarte	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 23/09/17 15:18
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+public class PlusHauteCarte extends Combination
+{
+    public PlusHauteCarte (ArrayList<Card> hand)
+    {
+        super(hand);
+    }
+}
