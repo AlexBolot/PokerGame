189224c1f5d09783331147aadf7cd93e96176f06
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-23 15:38:19 +0200

    Adding Suite class (unImplemented)

---
+++ src/main/java/TeamA/classe/Suite.java
@@ -0,0 +1,21 @@
+package TeamA.classe;
+
+import java.util.ArrayList;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The Suite	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 23/09/17 15:23
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+public class Suite extends Combination
+{
+    public Suite (ArrayList<Card> hand)
+    {
+        super(hand);
+    }
+}
