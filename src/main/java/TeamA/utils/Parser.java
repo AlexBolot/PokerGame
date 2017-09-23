970f78e120692b0e50ae81a085a0a15e36ed99e8
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-23 14:35:15 +0200

    Moving Parser to package utils

---
+++ src/main/java/TeamA/utils/Parser.java
@@ -0,0 +1,25 @@
+package TeamA.utils;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The Parser	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 23/09/17 14:34
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+public abstract class Parser
+{
+
+    public static void readCards ()
+    {
+
+    }
+
+    public static void parseCards ()
+    {
+
+    }
+}
