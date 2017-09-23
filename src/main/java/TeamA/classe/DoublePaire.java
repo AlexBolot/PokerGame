8ac0f46d4fc414cd0a56c61e139fc58e746bec9f
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-23 15:37:46 +0200

    Adding DoublePaire class (unImplemented)

---
+++ src/main/java/TeamA/classe/DoublePaire.java
@@ -0,0 +1,21 @@
+package TeamA.classe;
+
+import java.util.ArrayList;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The DoublePaire	 Class was Coded by : Alexandre BOLOT
+ .
+ . Last Modified : 23/09/17 15:23
+ .
+ . Contact : bolotalex06@gmail.com
+ ...............................................................................................................................*/
+
+public class DoublePaire extends Combination
+{
+    public DoublePaire (ArrayList<Card> hand)
+    {
+        super(hand);
+    }
+}
