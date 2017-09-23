e9538be2072a25a5c801758052a6fb76cd4fffed
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-24 01:17:53 +0200

    Adding Equals and toString methods for UnitTesting purposes

--- src/main/java/TeamA/classe/Card.java
+++ src/main/java/TeamA/classe/Card.java
@@ -11,33 +11,59 @@ package TeamA.classe;
  . -> Grégoire Peltier
  . -> Théos Mariani
  .
- . Last Modified : 23/09/17 16:42
+ . Last Modified : 24/09/17 01:17
  ...............................................................................................................................*/
 
-public class Card implements Comparable<Card> {
-    private int value;
+public class Card implements Comparable<Card>
+{
+    private int    value;
     private String name;
 
-    public Card(int value, String name){
+    public Card (int value, String name)
+    {
         this.value = value;
         this.name = name;
     }
 
-    public int compareTo(Card card) {
+    public int compareTo (Card card)
+    {
         return Integer.compare(this.value, card.value);
     }
 
-    public int getValue() {
+    public int getValue ()
+    {
         return value;
     }
-    public String getName() {
-        return name;
-    }
-    public void setValue(int value) {
+
+    public void setValue (int value)
+    {
         this.value = value;
     }
-    public void setName(String name) {
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public void setName (String name)
+    {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (obj == null || !(obj instanceof Card)) return false;
+
+        Card cmpCard = (Card) obj;
+
+        return this.getValue() == cmpCard.getValue() && this.getName().equals(cmpCard.getName());
+    }
+
+    @Override
+    public String toString ()
+    {
+        return getValue() + getName();
     }
 }
 
