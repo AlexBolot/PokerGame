eb9ad094d4ae1a15c2535c92ccd83f15b444af78
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-09-24 22:03:24 +0200

    Adding HandTest Unit Testing Class.

---
+++ src/test/java/TeamA/classe/HandTest.java
@@ -0,0 +1,182 @@
+package TeamA.classe;
+
+import TeamA.classe.Combinations.PlusHauteCarte;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+/*................................................................................................................................
+ . Copyright (c)
+ .
+ . The HandTest	 Class was Coded by : Team_A
+ .
+ . Members :
+ . -> Alexandre Bolot
+ . -> Mathieu Paillart
+ . -> Grégoire Peltier
+ . -> Théos Mariani
+ .
+ . Last Modified : 24/09/17 22:03
+ ...............................................................................................................................*/
+
+@SuppressWarnings ("Duplicates")
+public class HandTest
+{
+    private static ArrayList<Card> allCards = new ArrayList<>();
+    private        ArrayList<Card> cards    = new ArrayList<>();
+    private Hand hand;
+
+    @BeforeClass
+    public static void beforeClass ()
+    {
+        for (int i = 1; i < 14; i++)
+        {
+            allCards.add(new Card(i, ""));
+            allCards.add(new Card(i, ""));
+            allCards.add(new Card(i, ""));
+            allCards.add(new Card(i, ""));
+        }
+    }
+
+    @Before
+    public void before ()
+    {
+        cards.clear();
+
+        for (int i = 0; i < 5; i++)
+        {
+            int index = new Random().nextInt(allCards.size());
+            cards.add(allCards.get(index));
+        }
+
+        hand = new Hand(cards);
+    }
+
+    @Test
+    public void getHand () throws Exception
+    {
+        Assert.assertEquals(cards, hand.getHand());
+    }
+
+    @Test
+    public void setHand () throws Exception
+    {
+        ArrayList<Card> newCards = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            int index = new Random().nextInt(allCards.size());
+            newCards.add(allCards.get(index));
+        }
+
+        hand.setHand(newCards);
+
+        Assert.assertEquals(newCards, hand.getHand());
+    }
+
+    @Test
+    public void findBestCombination () throws Exception
+    {
+        //region PlusHauteCarte
+        Hand hand0 = new Hand(new ArrayList<>(Arrays.asList(new Card(2, "Ca"),
+                                                            new Card(5, "Co"),
+                                                            new Card(7, "Tr"),
+                                                            new Card(3, "Pi"),
+                                                            new Card(4, "Pi"))));
+        //endregion
+
+        //region Paire
+        Hand hand1 = new Hand(new ArrayList<>(Arrays.asList(new Card(6, "Ca"),
+                                                            new Card(2, "Co"),
+                                                            new Card(2, "Tr"),
+                                                            new Card(3, "Pi"),
+                                                            new Card(5, "Pi"))));
+        //endregion
+
+        //region DoublePaire
+        Hand hand2 = new Hand(new ArrayList<>(Arrays.asList(new Card(2, ""),
+                                                            new Card(3, ""),
+                                                            new Card(2, ""),
+                                                            new Card(3, ""),
+                                                            new Card(5, ""))));
+        //endregion
+
+        //region Brelan
+        Hand hand3 = new Hand(new ArrayList<>(Arrays.asList(new Card(2, "Co"),
+                                                            new Card(2, "Pi"),
+                                                            new Card(2, "Tr"),
+                                                            new Card(3, "Tr"),
+                                                            new Card(5, "Pi"))));
+        //endregion
+
+        //region Suite
+        Hand hand4 = new Hand(new ArrayList<>(Arrays.asList(new Card(2, "Ca"),
+                                                            new Card(3, "Tr"),
+                                                            new Card(4, "Ca"),
+                                                            new Card(5, "Co"),
+                                                            new Card(6, "Co"))));
+        //endregion
+
+        //region Couleur
+        Hand hand5 = new Hand(new ArrayList<>(Arrays.asList(new Card(5, "Ca"),
+                                                            new Card(6, "Ca"),
+                                                            new Card(7, "Ca"),
+                                                            new Card(3, "Ca"),
+                                                            new Card(5, "Ca"))));
+        //endregion
+
+        //region Full
+        Hand hand6 = new Hand(new ArrayList<>(Arrays.asList(new Card(2, "Tr"),
+                                                            new Card(2, "Ca"),
+                                                            new Card(2, "Co"),
+                                                            new Card(3, "Pi"),
+                                                            new Card(3, "Co"))));
+        //endregion
+
+        //region Carre
+        Hand hand7 = new Hand(new ArrayList<>(Arrays.asList(new Card(2, "Tr"),
+                                                            new Card(2, "Ca"),
+                                                            new Card(2, "Co"),
+                                                            new Card(2, "Pi"),
+                                                            new Card(3, "Co"))));
+        //endregion
+
+        //region QuinteFlush
+        Hand hand8 = new Hand(new ArrayList<>(Arrays.asList(new Card(2, "Co"),
+                                                            new Card(3, "Co"),
+                                                            new Card(4, "Co"),
+                                                            new Card(5, "Co"),
+                                                            new Card(6, "Co"))));
+        //endregion
+
+        Assert.assertTrue(hand0.findBestCombination() instanceof PlusHauteCarte);
+
+        //Will be used in future release
+        /*Assert.assertTrue(hand1.findBestCombination() instanceof Paire);
+        Assert.assertTrue(hand2.findBestCombination() instanceof DoublePaire);
+        Assert.assertTrue(hand3.findBestCombination() instanceof Brelan);
+        Assert.assertTrue(hand4.findBestCombination() instanceof Suite);
+        Assert.assertTrue(hand5.findBestCombination() instanceof Couleur);
+        Assert.assertTrue(hand6.findBestCombination() instanceof Full);
+        Assert.assertTrue(hand7.findBestCombination() instanceof Carre);
+        Assert.assertTrue(hand8.findBestCombination() instanceof QuiteFlush);*/
+    }
+
+    @Test
+    public void toStringTest () throws Exception
+    {
+        StringBuilder str = new StringBuilder();
+
+        for (Card c : hand.getHand())
+        {
+            str.append(c).append(" ");
+        }
+
+        Assert.assertEquals(str.toString(), hand.toString());
+    }
+}
