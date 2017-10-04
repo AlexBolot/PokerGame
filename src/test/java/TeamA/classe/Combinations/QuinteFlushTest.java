3132bb6a472ff46d3f4c8f3f070ec47c401ccfeb
Gregoire Peltier <gregoire.peltier@etu.unice.fr>
2017-10-04 17:17:51 +0200

    QuinteFlushTest.java     Implémentation du test de quinte flush compareto et istypeof

--- src/test/java/TeamA/classe/Combinations/QuinteFlushTest.java
+++ src/test/java/TeamA/classe/Combinations/QuinteFlushTest.java
@@ -2,10 +2,14 @@ package TeamA.classe.Combinations;
 
 import TeamA.classe.Card;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /*................................................................................................................................
  . Copyright (c)
@@ -21,34 +25,61 @@ import java.util.Arrays;
  . Last Modified : 26/09/17 17:03
  ...............................................................................................................................*/
 
-public class QuinteFlushTest
-{
+public class QuinteFlushTest {
+    private ArrayList<Card> card5Order;
+    private ArrayList<Card> cards2;
+    private ArrayList<Card> cards5Mixed;
+    private ArrayList<Card> cardsPair2;
+
+    @Before
+    public void init() {
+
+        card5Order = new ArrayList<>(
+                Arrays.asList(
+                        new Card(5, "Tr"),
+                        new Card(6, "Tr"),
+                        new Card(7, "Tr"),
+                        new Card(8, "Tr"),
+                        new Card(9, "Tr")));
+
+        cards2 = new ArrayList<>(Arrays.asList(new Card(2, "Co"),
+                new Card(4, "Co"),
+                new Card(6, "Co"),
+                new Card(5, "Co"),
+                new Card(3, "Co")));
+        cards5Mixed = new ArrayList<>(Arrays.asList(new Card(9, "Co"),
+                new Card(5, "Co"),
+                new Card(6, "Co"),
+                new Card(7, "Co"),
+                new Card(8, "Co")));
+        cardsPair2 = new ArrayList<>(Arrays.asList(new Card(2, "Ca"),
+                new Card(4, "Co"),
+                new Card(2, "Pi"),
+                new Card(5, "Tr"),
+                new Card(2, "Pi")));
+
+    }
+
     @Test
-    public void isTypeOf () throws Exception
-    {
-        QuinteFlush obj1 = new QuinteFlush(new ArrayList<>(Arrays.asList(new Card(5, "Tr"),
-                                                                         new Card(6, "Tr"),
-                                                                         new Card(7, "Tr"),
-                                                                         new Card(8, "Tr"),
-                                                                         new Card(9, "Tr"))));
+    public void testIsTypeOf() {
 
-        QuinteFlush obj2 = new QuinteFlush(new ArrayList<>(Arrays.asList(new Card(2, "Co"),
-                                                                         new Card(4, "Co"),
-                                                                         new Card(6, "Co"),
-                                                                         new Card(5, "Co"),
-                                                                         new Card(3, "Co"))));
+        assertTrue(QuinteFlush.isTypeOf(cards2));
+        assertFalse(QuinteFlush.isTypeOf(cardsPair2));
+    }
 
-        QuinteFlush obj3 = new QuinteFlush(new ArrayList<>(Arrays.asList(new Card(9, "Co"),
-                                                                         new Card(5, "Co"),
-                                                                         new Card(6, "Co"),
-                                                                         new Card(7, "Co"),
-                                                                         new Card(8, "Co"))));
+    @Test
+    public void testCompareTo() throws Exception {
 
-        // obj1 > obj2
-        Assert.assertEquals(1, obj1.compareTo(obj2));
-        // obj2 < obj1
-        Assert.assertEquals(-1, obj2.compareTo(obj1));
-        // obj1 = obj3
-        Assert.assertEquals(0, obj1.compareTo(obj3));
+        QuinteFlush flush5 = new QuinteFlush(
+                card5Order);
+        QuinteFlush flush2 = new QuinteFlush(cards2);
+
+        QuinteFlush flush5Co = new QuinteFlush(cards5Mixed);
+        // flush5 > flush2
+        Assert.assertEquals(1, flush5.compareTo(flush2));
+        // flush2 < flush5
+        Assert.assertEquals(-1, flush2.compareTo(flush5));
+        // flush5 = flush5Co
+        Assert.assertEquals(0, flush5.compareTo(flush5Co));
     }
 }
