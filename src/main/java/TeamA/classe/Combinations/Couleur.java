fe54473dcc8c667c784c9929541a55ebfaf439bb
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-10-05 12:06:34 +0200

    Adding toString method

--- src/main/java/TeamA/classe/Combinations/Couleur.java
+++ src/main/java/TeamA/classe/Combinations/Couleur.java
@@ -3,7 +3,6 @@ package TeamA.classe.Combinations;
 import TeamA.classe.Card;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /*................................................................................................................................
  . Copyright (c)
@@ -16,33 +15,43 @@ import java.util.HashMap;
  . -> Grégoire Peltier
  . -> Théos Mariani
  .
- . Last Modified : 23/09/17 16:42
+ . Last Modified : 05/10/17 12:06
  ...............................................................................................................................*/
 
 public class Couleur extends Combination
 {
     private String color;
+
     public Couleur (ArrayList<Card> hand)
     {
         super(hand);
         this.color = Couleur.FindColor(hand);
         this.setValue(5);
     }
-    public String getColor(){
-        return color;
-    }
-    public static boolean isTypeOf(ArrayList<Card> hand){
+
+    public static boolean isTypeOf (ArrayList<Card> hand)
+    {
         return Couleur.FindColor(hand) != null;
     }
-    public static String FindColor(ArrayList<Card> hand){
+
+    public static String FindColor (ArrayList<Card> hand)
+    {
         String color = hand.get(1).getName();
-        for(Card c : hand){
-            if(!color.equals(c.getName())){
+        for (Card c : hand)
+        {
+            if (!color.equals(c.getName()))
+            {
                 return null;
             }
         }
         return color;
     }
+
+    public String getColor ()
+    {
+        return color;
+    }
+
     @Override
     public int compareTo (Combination o)
     {
@@ -55,5 +64,11 @@ public class Couleur extends Combination
         PlusHauteCarte phc2 = new PlusHauteCarte(couleur.getRestOfCards());
 
         return phc1.compareTo(phc2);
+    }
+
+    @Override
+    public String toString ()
+    {
+        return this.getClass().getSimpleName() + " avec carte haute : " + gethighestcard(getRestOfCards());
     }
 }
