fae9578f54dfe56b4117367a99d46ff0f4abad33
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-10-06 21:44:55 +0200

    Suite : adding exceptions and upgrading testings

--- src/main/java/TeamA/classe/Combinations/Suite.java
+++ src/main/java/TeamA/classe/Combinations/Suite.java
@@ -16,43 +16,52 @@ import java.util.Collections;
  . -> Grégoire Peltier
  . -> Théos Mariani
  .
- . Last Modified : 23/09/17 16:42
+ . Last Modified : 06/10/17 21:44
  ...............................................................................................................................*/
 
+@SuppressWarnings ("unchecked")
 public class Suite extends Combination
 {
     private Card start;
 
-    public Card getStart() {
-        return start;
-    }
-
-    public void setStart(Card start) {
-        this.start = start;
-
-    }
-
-    public Suite (ArrayList<Card> hand)
+    public Suite (ArrayList<Card> hand) throws IllegalArgumentException
     {
+        //Has to be done at first line...
         super(hand);
-        start=FindSuite(hand);
+
+        Card start = FindSuite(hand);
+
+        if (start == null) throw new IllegalArgumentException("Parameter does not contain a " + getClass().getSimpleName());
+
+        this.start = start;
         setValue(5);
     }
-    public static boolean isTypeOf(ArrayList<Card> cards){
 
-        return(FindSuite(cards)!=null);
+    public static boolean isTypeOf (ArrayList<Card> cards) throws IllegalArgumentException
+    {
+        if (cards == null) throw new IllegalArgumentException("Parameter is null");
+        if (cards.size() != 5) throw new IllegalArgumentException("Invalid size of param : " + cards.size() + " != 5");
+
+        return (FindSuite(cards) != null);
     }
-    public static Card FindSuite(ArrayList<Card> cards) {
-        Collections.sort(cards);
-        int s=0;
-        for (int i = 0; i < cards.size()-1; i++) {
 
-            if (cards.get(i).getValue()+1==cards.get(i+1).getValue()){
+    public static Card FindSuite (ArrayList<Card> cards) throws IllegalArgumentException
+    {
+        if (cards == null) throw new IllegalArgumentException("Parameter is null");
+        if (cards.size() != 5) throw new IllegalArgumentException("Invalid size of param : " + cards.size() + " != 5");
+
+        Collections.sort(cards);
+        int s = 0;
+        for (int i = 0; i < cards.size() - 1; i++)
+        {
+            if (cards.get(i).getValue() + 1 == cards.get(i + 1).getValue())
+            {
                 s++;
             }
         }
-        if (s==4) return cards.get(0);
-        if (cards.get(4).getValue() == 14) {
+        if (s == 4) return cards.get(0);
+        if (cards.get(4).getValue() == 14)
+        {
             ArrayList<Card> clone = (ArrayList<Card>) cards.clone();
             clone.get(4).setValue(1);
             return FindSuite(clone);
@@ -60,12 +69,30 @@ public class Suite extends Combination
         return null;
     }
 
+    public Card getStart ()
+    {
+        return start;
+    }
+
+    public void setStart (Card start)
+    {
+        this.start = start;
+    }
 
     @Override
-    public int compareTo(Combination o) {
+    public int compareTo (Combination o) throws IllegalArgumentException
+    {
+        if (o == null) throw new IllegalArgumentException("Parameter is null");
+
         int valueCompare = super.compareTo(o);
         if (valueCompare != 0) return valueCompare;
         Suite s = (Suite) o;
         return start.compareTo(s.getStart());
+    }
+
+    @Override
+    public String toString ()
+    {
+        return this.getClass().getSimpleName() + " qui commence à " + this.getStart();
     }
 }
--- src/test/java/TeamA/classe/Combinations/SuiteTest.java
+++ src/test/java/TeamA/classe/Combinations/SuiteTest.java
@@ -1,16 +1,14 @@
 package TeamA.classe.Combinations;
 
 import TeamA.classe.Card;
-import jdk.internal.util.xml.impl.Pair;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /*................................................................................................................................
  . Copyright (c)
@@ -23,11 +21,11 @@ import static org.junit.Assert.assertTrue;
  . -> Grégoire Peltier
  . -> Théos Mariani
  .
- . Last Modified : 26/09/17 16:50
+ . Last Modified : 06/10/17 21:44
  ...............................................................................................................................*/
 
-public class SuiteTest {
-
+public class SuiteTest
+{
     private ArrayList<Card> cards5start;
     private ArrayList<Card> cards2Start;
     private ArrayList<Card> cards5startMixed;
@@ -36,67 +34,130 @@ public class SuiteTest {
     private ArrayList<Card> cardsAstart;
 
     @Before
-    public void init() {
-
+    public void init ()
+    {
         cards5start = new ArrayList<>(Arrays.asList(new Card(5, "Ca"),
-                new Card(6, "Co"),
-                new Card(7, "Tr"),
-                new Card(8, "Pi"),
-                new Card(9, "Pi")));
-        cardsAstart = new ArrayList<>(Arrays.asList(
-                new Card(14, "Ca"),
-                new Card(2, "Co"),
-                new Card(3, "Tr"),
-                new Card(4, "Pi"),
-                new Card(5, "Pi")));
+                                                    new Card(6, "Co"),
+                                                    new Card(7, "Tr"),
+                                                    new Card(8, "Pi"),
+                                                    new Card(9, "Pi")));
+
+        cardsAstart = new ArrayList<>(Arrays.asList(new Card(14, "Ca"),
+                                                    new Card(2, "Co"),
+                                                    new Card(3, "Tr"),
+                                                    new Card(4, "Pi"),
+                                                    new Card(5, "Pi")));
+
         cards2Start = new ArrayList<>(Arrays.asList(new Card(2, "Ca"),
-                new Card(4, "Co"),
-                new Card(6, "Tr"),
-                new Card(5, "Pi"),
-                new Card(3, "Pi")));
+                                                    new Card(4, "Co"),
+                                                    new Card(6, "Tr"),
+                                                    new Card(5, "Pi"),
+                                                    new Card(3, "Pi")));
+
         cards5startMixed = new ArrayList<>(Arrays.asList(new Card(9, "Ca"),
-                new Card(5, "Co"),
-                new Card(6, "Tr"),
-                new Card(7, "Pi"),
-                new Card(8, "Pi")));
+                                                         new Card(5, "Co"),
+                                                         new Card(6, "Tr"),
+                                                         new Card(7, "Pi"),
+                                                         new Card(8, "Pi")));
+
         cards2Pair = new ArrayList<>(Arrays.asList(new Card(9, "Ca"),
-                new Card(11, "Co"),
-                new Card(6, "Tr"),
-                new Card(2, "Pi"),
-                new Card(2, "Pi")));
+                                                   new Card(11, "Co"),
+                                                   new Card(6, "Tr"),
+                                                   new Card(2, "Pi"),
+                                                   new Card(2, "Pi")));
+
         cards10Carree = new ArrayList<>(Arrays.asList(new Card(9, "Ca"),
-                new Card(10, "Co"),
-                new Card(10, "Tr"),
-                new Card(10, "Pi"),
-                new Card(10, "Ca")));
+                                                      new Card(10, "Co"),
+                                                      new Card(10, "Tr"),
+                                                      new Card(10, "Pi"),
+                                                      new Card(10, "Ca")));
     }
 
+    //region //============= Constructor (x5) =============//
     @Test
-    public void testIsTypeOf() {
+    public void constructor_Right ()
+    {
+        Suite suite = new Suite(cards5start);
+        assertEquals(new Card(5, "Ca"), suite.getStart());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void constructor_EmptyList ()
+    {
+        Suite suite = new Suite(new ArrayList<Card>());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void constructor_NullParam ()
+    {
+        Suite suite = new Suite(null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void constructor_WrongSize ()
+    {
+        //Here the size is 6 > 5
+        Suite suite = new Suite(new ArrayList<>(Arrays.asList(new Card(9, "Ca"),
+                                                              new Card(3, "Co"),
+                                                              new Card(8, "Co"),
+                                                              new Card(5, "Pi"),
+                                                              new Card(7, "Tr"),
+                                                              new Card(5, "Ca"))));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void constructor_WrongCards ()
+    {
+        Suite suite = new Suite(cards2Pair);
+    }
+    //endregion ===========================================//
+
+    //region //============== isTypeOf (x4) ===============//
+    @Test
+    public void isTypeOf_Right ()
+    {
         //Test Right
         assertFalse(Suite.isTypeOf(cards10Carree));
         assertTrue(Suite.isTypeOf(cards5start));
         assertTrue(Suite.isTypeOf(cards5startMixed));
         assertTrue(Suite.isTypeOf(cardsAstart));
-
     }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void isTypeOf_EmptyList ()
+    {
+        Suite.isTypeOf(new ArrayList<Card>());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void isTypeOf_NotGoodSize ()
+    {
+        //Here the size is 6 > 5
+        Suite.isTypeOf(new ArrayList<>(Arrays.asList(new Card(9, "Ca"),
+                                                     new Card(3, "Co"),
+                                                     new Card(8, "Co"),
+                                                     new Card(5, "Pi"),
+                                                     new Card(7, "Tr"),
+                                                     new Card(5, "Ca"))));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void isTypeOf_NullParam ()
+    {
+        Suite.isTypeOf(null);
+    }
+    //endregion ===========================================//
+
+    //region //============== compareTo (x2) ==============//
     @Test
-    public void testCompareTo() throws Exception {
+    public void compareTo_Right () throws Exception
+    {
         Suite suite5Start = new Suite(cards5start);
-
         Suite suite2Start = new Suite(cards2Start);
-
         Suite suite5Mixed = new Suite(cards5startMixed);
-
         Suite suiteA = new Suite(cardsAstart);
-
         Paire paire = new Paire(cards2Pair);
-
         Carre carre = new Carre(cards10Carree);
-        //********************************************************
-        // testRight
-        //********************************************************
 
         // suite5Start > suite2Start
         Assert.assertEquals(1, suite5Start.compareTo(suiteA));
@@ -110,4 +171,22 @@ public class SuiteTest {
         Assert.assertEquals(-1, suite5Mixed.compareTo(carre));
     }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void compareTo_NullParam ()
+    {
+        Suite suite5Start = new Suite(cards5start);
+
+        suite5Start.compareTo(null);
+    }
+    //endregion ===========================================//
+
+    //region //================= toString =================//
+    @Test
+    public void ToString ()
+    {
+        Suite suite = new Suite(cards5start);
+
+        assertEquals("Suite qui commence à 5Ca", suite.toString());
+    }
+    //endregion ===========================================//
 }
