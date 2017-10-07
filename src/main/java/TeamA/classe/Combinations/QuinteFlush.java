a90e6f8f7750981d42722071a8913e5e61d1381a
Gregoire Peltier <gregoire.peltier@etu.unice.fr>
2017-10-07 18:31:14 +0200

    QuinteFlush     Ajout des exception pour le constructeur, compareTo, istypeOf et findquiteflush

--- src/main/java/TeamA/classe/Combinations/QuinteFlush.java
+++ src/main/java/TeamA/classe/Combinations/QuinteFlush.java
@@ -24,20 +24,20 @@ public class QuinteFlush extends Combination
 {
     private Card start;
 
-    public Card getStart() {
-        return start;
-    }
 
-    public void setStart(Card start) {
-        this.start = start;
-    }
-    public QuinteFlush (ArrayList<Card> hand)
+    public QuinteFlush(ArrayList<Card> hand) throws IllegalArgumentException
     {
         super(hand);
         start=FindQuinteFlush(hand);
+        if (start == null)
+            throw new IllegalArgumentException("Parameter does not contain a " + getClass().getSimpleName());
         setValue(8);
     }
-    public static Card FindQuinteFlush(ArrayList<Card> cards) {
+
+    public static Card FindQuinteFlush(ArrayList<Card> cards) throws IllegalArgumentException {
+        if (cards == null) throw new IllegalArgumentException("Parameter is null");
+        if (cards.size() != 5) throw new IllegalArgumentException("Invalid size of param : " + cards.size() + " != 5");
+
         if (Couleur.isTypeOf(cards)) {
             if (Suite.isTypeOf(cards)) {
                 return (FindSuite(cards));
@@ -50,12 +50,20 @@ public class QuinteFlush extends Combination
     }
 
     @Override
-    public int compareTo(Combination o) {
+    public int compareTo(Combination o) throws IllegalArgumentException {
+        if (o == null) throw new IllegalArgumentException("Parameter is null");
         int comCompare = super.compareTo(o);
         if (comCompare != 0) return comCompare;
         return this.getStart().compareTo(((QuinteFlush) o).getStart());
     }
 
+    public Card getStart() {
+        return start;
+    }
+
+    public void setStart(Card start) {
+        this.start = start;
+    }
     @Override
     public String toString () {
         return this.getClass().getSimpleName() + " qui commence à " + this.getStart();
